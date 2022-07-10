96bff42d4b49db77381a8e6e0a3182436506c6d0
xantoz-vrc <xantoz-vrc@nowhere.com>
2022-07-10 12:08:52 +0200

    DiceDebug: Make it work under vket rules (unsynced)
    It's unsynced under vket rules, since of how it worked we simply
    synced the string.

--- Scripts/DiceDebug.cs
+++ Scripts/DiceDebug.cs
@@ -11,7 +11,11 @@ namespace Vket2022Summer.Circle314
 namespace XZDice
 #endif
 {
+#if VITDECK_HIDE_MENUITEM
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+#else
     [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+#endif
     public class DiceDebug : UdonSharpBehaviour
     {
         [SerializeField]
@@ -23,11 +27,21 @@ namespace XZDice
         [SerializeField]
         private Text text = null;
 
+#if VITDECK_HIDE_MENUITEM
+#else
         [UdonSynced]
+#endif
         private string output;
 
+#if VITDECK_HIDE_MENUITEM
+        public void _VketStart()
+#else
         private void Start()
+#endif
         {
+#if VITDECK_HIDE_MENUITEM
+            UpdateText("Start");
+#else
             if (textMeshPro == null) {
                 textMeshPro = GetComponent<TextMeshProUGUI>();
             }
@@ -36,12 +50,12 @@ namespace XZDice
                 text = GetComponent<Text>();
             }
 
+            if (Networking.IsOwner(gameObject))
+                UpdateText("Start");
+#endif
             foreach (Die die in dice) {
                 die._AddListener(this);
             }
-
-            if (Networking.IsOwner(gameObject))
-                UpdateText("Start");
         }
 
         // DiceListener
@@ -62,10 +76,13 @@ namespace XZDice
             UpdateText("DiceResult");
         }
 
+#if VITDECK_HIDE_MENUITEM
+#else
         public override void OnDeserialization()
         {
             ApplyText();
         }
+#endif
 
         private void ApplyText()
         {
@@ -80,8 +97,11 @@ namespace XZDice
 
         private void UpdateText(string extra)
         {
+#if VITDECK_HIDE_MENUITEM
+#else
             if (!Networking.IsOwner(gameObject) && Networking.LocalPlayer != null)
                 Networking.SetOwner(Networking.LocalPlayer, gameObject);
+#endif
 
             string playerName = (Networking.LocalPlayer != null) ? Networking.LocalPlayer.displayName : "";
             output = extra + "\n" + "Thrower: " + playerName + "\n";
@@ -90,7 +110,10 @@ namespace XZDice
             }
 
             ApplyText();
+#if VITDECK_HIDE_MENUITEM
+#else
             RequestSerialization();
+#endif
         }
     }
 }
